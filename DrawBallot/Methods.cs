1a612e075bff9996c053dc3476f788cef8726c25
Huynh Brooky <brookyct95@gmail.com>
2017-06-16 16:39:34 +0700

    Add project files.

---
+++ DrawBallot/Methods.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.IO;
+using System.Text.RegularExpressions;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+namespace DrawBallot
+{
+    static class Methods
+    {
+        private static Random rng = new Random();
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+        public static string DataTableToCSV(this DataTable datatable, char seperator)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (DataRow dr in datatable.Rows)
+            {
+                for (int i = 0; i < datatable.Columns.Count; i++)
+                {
+                    sb.Append(dr[i].ToString());
+
+                    if (i < datatable.Columns.Count - 1)
+                        sb.Append(seperator);
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+
+        public static DataTable ConvertCSVtoDataTable1(string strFilePath)
+        {
+            StreamReader sr = new StreamReader(strFilePath);
+            DataTable dt = new DataTable();
+            var tempvalue ="";
+
+            dt.Columns.Add("ID", typeof(string));
+            dt.Columns.Add("isDrawed", typeof(bool));
+            int index = 0;
+            while (!sr.EndOfStream)
+            {
+                tempvalue = sr.ReadLine();
+                string[] rows = new string[tempvalue.Split(';').Length];
+                index = 0;
+                foreach (string value in tempvalue.Split(';'))
+                {
+                    rows[index] = value;
+                    index++;
+                }
+                
+                DataRow dr = dt.NewRow();
+                string rowValue = null;
+                for (int i = 0; i < rows.Length-1; i++)
+                {       
+                    rowValue += rows[i];
+                    rowValue += '-';
+                }
+                rowValue = rowValue.Substring(0, rowValue.Length - 1);
+                dr[0] = rowValue;
+                dr[1] = ToBoolean(rows[rows.Length-1]);
+                
+                dt.Rows.Add(dr);
+            }
+            sr.Close();
+            return dt;
+        }
+
+        public static DataTable ConvertCSVtoDataTable2(string strFilePath)
+        {
+            StreamReader sr = new StreamReader(strFilePath);
+            
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("First Name");
+            dt.Columns.Add("Last Name");
+            dt.Columns.Add("Prize");
+            dt.Columns.Add("Date");
+            while (!sr.EndOfStream)
+            {
+                string[] rows = Regex.Split(sr.ReadLine(), ";");
+                DataRow dr = dt.NewRow();
+                for (int i = 0; i < 5; i++)
+                {
+                        dr[i] = rows[i];
+                }
+                dt.Rows.Add(dr);
+            }
+            sr.Close();
+            return dt;
+        }
+
+        public static string ExcelToCSV(string FilePath, char Seperator)
+        {
+            StringBuilder sb = new StringBuilder();
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(FilePath);
+            Excel._Worksheet xlWorksheet = (Excel._Worksheet)xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int RowCount = xlRange.Rows.Count;
+            int ColCount = xlRange.Columns.Count;
+            for (int i = 1; i <= RowCount; i++)
+            {
+                for (int j = 1; j <= ColCount; j++)
+                {
+                    sb.Append(xlRange.Cells[i,j].value2.ToString());
+                    if (j <= ColCount)
+                        sb.Append(Seperator);
+                }
+                sb.Append("false");
+                sb.AppendLine();
+            }
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+            return sb.ToString();
+        }
+        public static bool ToBoolean(this string value)
+        {
+            switch (value.ToLower())
+            {
+                case "true":
+                    return true;
+                case "t":
+                    return true;
+                case "1":
+                    return true;
+                case "0":
+                    return false;
+                case "false":
+                    return false;
+                case "f":
+                    return false;
+                default:
+                    throw new InvalidCastException("You can't cast a weird value to a bool!");
+            }
+        }
+        public static ListViewItem CreateItem(string[] arr)
+        {
+            ListViewItem item = new ListViewItem(arr);
+            return item;
+        }
+    }
+    
+}
---
+++ DrawBallot/Setting.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace DrawBallot
+{
+    public partial class Setting : Form
+    {
+        Form _form = null;
+        DataTable mDataTable = new DataTable();
+        int Mode = 0;
+        public BindingList<string> listPrize = new BindingList<string>();
+        public Setting()
+        {
+
+            InitializeComponent();
+            boxID.Visible = false;
+            boxFN.Visible = false;
+            boxLN.Visible = false;
+            bCon1.Visible = false;
+            bCan1.Visible = false;
+
+            boxPrize.Visible = false;
+            bCon2.Visible = false;
+            bCan2.Visible = false;
+
+            listPrize.Add("First Place");
+            listPrize.Add("Second Place");
+            listPrize.Add("Third Place");
+            listBox1.DataSource = listPrize;
+            
+
+            mDataTable.Columns.Add("ID", typeof(string));
+            mDataTable.Columns.Add("First Name", typeof(string));
+            mDataTable.Columns.Add("Last Name", typeof(string));
+            mDataTable.Columns.Add("isDrawed", typeof(bool));
+            mDataTable.Columns["isDrawed"].DefaultValue = false;
+            dataGridView1.DataSource = mDataTable;
+
+            if (File.Exists("Participant.csv"))
+            {
+                mDataTable = Methods.ConvertCSVtoDataTable1("Participant.csv");
+            }
+            else
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Title = "Choose data source";
+                ofd.Filter = "Excel File (*.xlsx)|*.xlsx|07-2003 Excel File (*.xls)|*.xls|all file (*.*)|*.*";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText("Participant.csv", Methods.ExcelToCSV(ofd.FileName, ';').ToString());
+                    mDataTable = Methods.ConvertCSVtoDataTable1("Participant.csv");
+                    MessageBox.Show("Loading Done");
+                }
+            }
+
+        }
+
+        private void Setting_Load(object sender, EventArgs e)
+        {
+            //string mDbPath = Application.StartupPath + "/DatabaseFinal.sqlite";
+            //mConn = new SQLiteConnection("Data Source=" + mDbPath);
+            //mConn.Open();
+            //mAdapter = new SQLiteDataAdapter("SELECT * FROM [PARTICIPANT]", mConn);
+            //mAdapter.Fill(mDataTable);
+
+            //new SQLiteCommandBuilder(mAdapter);
+            dataGridView1.DataSource = mDataTable;
+        }
+
+        /*public static List<string> ExcelReader(string fileLocation)
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(fileLocation);
+            xlWorkbook.SaveAs( fileLocation + ".csv", Excel.XlFileFormat.xlCSVWindows);
+            xlWorkbook.Close(true);
+            xlApp.Quit();
+            List<string> valueList = null;
+            using (StreamReader sr = new StreamReader(fileLocation + ".csv"))
+            {
+                string content = sr.ReadToEnd();
+                valueList = new List<string>(
+                    content.Split(
+                        new string[] { "\r\n" },
+                        StringSplitOptions.RemoveEmptyEntries
+                    )
+                );
+            }
+            new FileInfo(fileLocation + ".csv").Delete();
+            return valueList;
+        }*/
+
+        private void bLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel File (*.xlsx)|*.xlsx|07-2003 Excel File (*.xls)|*.xls|all file (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText("Participant.csv", Methods.ExcelToCSV(ofd.FileName, ';').ToString());
+                mDataTable = Methods.ConvertCSVtoDataTable1("Participant.csv");
+                MessageBox.Show("Loading Done");
+                dataGridView1.DataSource = mDataTable;
+            }
+        }
+
+        private void bAdd_Click(object sender, EventArgs e)
+        {
+            boxID.Text = null;
+            boxFN.Text = null;
+            boxLN.Text = null;
+            boxID.Visible = true;
+            boxFN.Visible = true;
+            boxLN.Visible = true;
+            bAdd1.Enabled = false;
+            bDel1.Enabled = false;
+            bMod1.Enabled = false;
+            bLoad1.Enabled = false;
+            bCon1.Visible = true;
+            bCan1.Visible = true;
+            Mode = 1;
+        }
+
+        private void bDel_Click(object sender, EventArgs e)
+        {
+            int selectedRow;
+            selectedRow = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(selectedRow);
+        }
+
+        private void bMod_Click(object sender, EventArgs e)
+        {
+            int selectedRow;
+            selectedRow = dataGridView1.CurrentCell.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[selectedRow];
+            boxID.Text = row.Cells["ID"].Value.ToString();
+            boxFN.Text = row.Cells["First Name"].Value.ToString();
+            boxLN.Text = row.Cells["Last Name"].Value.ToString();
+            boxID.Visible = true;
+            boxFN.Visible = true;
+            boxLN.Visible = true;
+            bAdd1.Enabled = false;
+            bDel1.Enabled = false;
+            bMod1.Enabled = false;
+            bLoad1.Enabled = false;
+            bCon1.Visible = true;
+            bCan1.Visible = true;
+            Mode = 2;
+        }
+
+        private void bReset_Click(object sender, EventArgs e)
+        {
+            mDataTable.Clear();
+        }
+
+        private void bCon_Click(object sender, EventArgs e)
+        {
+            if (Mode == 1)
+            {
+                var newrow = mDataTable.NewRow();
+                newrow[0] = boxID.Text;
+                newrow[1] = boxFN.Text;
+                newrow[2] = boxLN.Text;
+                mDataTable.Rows.Add(newrow);
+            }
+            else
+            {
+                int selectedRow;
+                selectedRow = dataGridView1.CurrentCell.RowIndex;
+                DataGridViewRow row = dataGridView1.Rows[selectedRow];
+                row.Cells[0].Value = boxID.Text;
+                row.Cells[1].Value = boxFN.Text;
+                row.Cells[2].Value = boxLN.Text;
+
+                dataGridView1.EndEdit();
+                ((DataRowView)dataGridView1.CurrentRow.DataBoundItem).EndEdit();
+            }
+
+            boxID.Visible = false;
+            boxFN.Visible = false;
+            boxLN.Visible = false;
+            bAdd1.Enabled = true;
+            bDel1.Enabled = true;
+            bMod1.Enabled = true;
+            bLoad1.Enabled = true;
+            bCon1.Visible = false;
+            bCan1.Visible = false;
+            //mAdapter.Update(mDataTable);
+        }
+
+        private void bCan_Click(object sender, EventArgs e)
+        {
+            boxID.Visible = false;
+            boxFN.Visible = false;
+            boxLN.Visible = false;
+            bAdd1.Enabled = true;
+            bDel1.Enabled = true;
+            bMod1.Enabled = true;
+            bLoad1.Enabled = true;
+            bCon1.Visible = false;
+            bCan1.Visible = false;
+            Mode = 0;
+        }
+
+        private void bAdd2_Click(object sender, EventArgs e)
+        {
+            
+            bAdd2.Enabled = false;
+            bDel2.Enabled = false;
+            bMod2.Enabled = false;
+            bReset2.Enabled = false;
+
+            boxPrize.Visible = true;
+            bCon2.Visible = true;
+            bCan2.Visible = true;
+            Mode = 1;
+        }
+
+        private void bMod2_Click(object sender, EventArgs e)
+        {
+            bAdd2.Enabled = false;
+            bDel2.Enabled = false;
+            bMod2.Enabled = false;
+            bReset2.Enabled = false;
+
+            boxPrize.Visible = true;
+            bCon2.Visible = true;
+            bCan2.Visible = true;
+            Mode = 2;
+        }
+
+        private void bCon2_Click(object sender, EventArgs e)
+        {
+            if (Mode == 1)
+            {
+                listPrize.Add(boxPrize.Text);
+            }
+            else
+            {
+                listPrize[listBox1.SelectedIndex]= boxPrize.Text;
+            }
+            bAdd2.Enabled = true;
+            bDel2.Enabled = true;
+            bMod2.Enabled = true;
+            bReset2.Enabled = true;
+
+            boxPrize.Visible = false;
+            bCon2.Visible = false;
+            bCan2.Visible = false;
+            Mode = 0;
+        }
+
+        private void bBrowsePic_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog ofd = new OpenFileDialog();
+            //try
+            //{
+              //  if (ofd.ShowDialog() == DialogResult.OK)
+                //{
+                  //  Bitmap image = new Bitmap(ofd.OpenFile());
+                    
+                //}
+            //}
+            //catch (Exception ex)
+            //{
+              //  MessageBox.Show(ex.Message);
+            //}
+        }
+
+        private void bCan2_Click(object sender, EventArgs e)
+        {
+            bAdd2.Enabled = true;
+            bDel2.Enabled = true;
+            bMod2.Enabled = true;
+            bReset2.Enabled = true;
+
+            boxPrize.Visible = false;
+            bCon2.Visible = false;
+            bCan2.Visible = false;
+            Mode = 0;
+        }
+
+        private void bReset2_Click(object sender, EventArgs e)
+        {
+            listPrize.Clear();
+            listPrize.Add("First Place");
+            listPrize.Add("Second Place");
+            listPrize.Add("Third Place");
+        }
+
+        private void bUp_Click(object sender, EventArgs e)
+        {
+            int n = listBox1.SelectedIndex;
+            string temp = listPrize[n];
+            if (n >0)
+            {
+                listPrize.RemoveAt(n);
+                listPrize.Insert(n - 1, temp);
+                listBox1.SetSelected(n - 1,true);
+            }
+        }
+
+        private void bDown_Click(object sender, EventArgs e)
+        {
+            int n = listBox1.SelectedIndex;
+            string temp = listPrize[n];
+            if (n < listPrize.Count - 1)
+            {
+                listPrize.RemoveAt(n);
+                listPrize.Insert(n + 1, temp);
+                listBox1.SetSelected(n + 1, true);
+            }
+        }
+
+        private void bDel2_Click(object sender, EventArgs e)
+        {
+            listPrize.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        public DataTable Table
+        {
+            get
+            {
+                return this.mDataTable;
+            }
+            set
+            {
+                this.mDataTable = value;
+            }
+        }
+    }
+}
